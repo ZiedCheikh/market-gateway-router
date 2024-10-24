15bddb4a82e5847719a0b93b10f1f5b15ef69835
ZiedEcheikh <echeikh.zied@gmail.com>
2024-10-24 18:47:50 +0200

    :stethoscope: enable healthcheck

--- src/main/java/tu/startup/market/gateway/SecurityConfig.java
+++ src/main/java/tu/startup/market/gateway/SecurityConfig.java
@@ -5,17 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
+        http.securityMatcher(new NegatedServerWebExchangeMatcher(
+                        ServerWebExchangeMatchers.pathMatchers("/actuator/health")))
                 .authorizeExchange((authorize) -> authorize.anyExchange().authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults());
         return http.build();
     }
+
 
 }
